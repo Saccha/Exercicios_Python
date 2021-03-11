0fffd334b52cf3b862cb9aa998a3ffb0b6bfad78
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 13:38:39 -0300

    Create ex31.py

---
+++ secao04/ex31.py
@@ -0,0 +1,3 @@
+#31. Leia um número inteiro e imprima o seu antecessor e o seu sucessor.
+n = int(input('Digite um número inteiro para saber seu antecessor: '))
+print('O antecessor de {0} é {1}'.format(n,n-1))
