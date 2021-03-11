5c428aea13143f8536e7e92b9f8d34d9cf9846ba
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 15:39:59 -0300

    Create ex33.py

---
+++ secao04/ex33.py
@@ -0,0 +1,6 @@
+#33.  Leia o tamanho do lado de um quadrado e imprima como resultado a sua área.
+
+
+L = int(input('Digite um dos lados do quadrado para saber sua área: '))
+A = L*L
+print('A área do quadrado é de {}'.format(A))
