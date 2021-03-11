acaefd62fb39823ca879a5fb75fcc00f810e9c54
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 01:50:46 -0300

    Create ex19.py

---
+++ secao04/ex19.py
@@ -0,0 +1,6 @@
+""" 19. Leia um valor de volume em litros e apresente-o convertido em metros cúbicos m³ 
+fórmula de conversão é: M = L/1000 sendo L o volume em litros e M c volume em metros cúbicos."""
+
+l = float(input("Digite o valor: "))
+m = l/1000
+print(m)
