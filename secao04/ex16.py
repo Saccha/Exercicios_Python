c24d00dd7bf06f8d62fed255de751ea00440d145
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 01:49:09 -0300

    Create ex16.py

---
+++ secao04/ex16.py
@@ -0,0 +1,7 @@
+""" 16. Leia um valor de comprimento em polegadas e apresente-o convertido em centímetros.
+A fórmula de conversão é: C = P * 2,54, sendo C o comprimento em centímetros e P o
+comprimento em polegadas."""
+
+p = float(input("Digite o valor de G: "))
+c = p * 2.54
+print("%.4f" % c)
