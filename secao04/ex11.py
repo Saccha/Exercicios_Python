32aea38553a2d463c6a1c755443b7c672ec63f2b
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 01:09:19 -0300

    Create ex11.py

---
+++ secao04/ex11.py
@@ -0,0 +1,7 @@
+""" 11. Leia uma velocidade em m/s (metros por segundo) e apresente-a convertida em km/h
+(quilómetros por hora). A fórmula de conversão é: K = M*3,6, sendo K a velocidade
+em km/h e M em rn/s."""
+
+m = float(input('Digite o valor de m: '))
+k = m * 3.6
+print(k)
