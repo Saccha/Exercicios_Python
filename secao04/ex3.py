cbc4a1bab354bd1cb2b70bceb3c11472030b1164
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-10 23:26:10 -0300

    Create ex3.py

---
+++ secao04/ex3.py
@@ -0,0 +1,6 @@
+" 3. Peça ao usuário para digitar três valores inteiros e imprima a soma deles."
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite um numero: '))
+numero3 = int(input('Digite um numero: '))
+soma = (numero1 + numero2 + numero3)
+print(soma)
