7fde1ddc49af5987ad0dec92bb91d8c24d9ea8d6
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 13:34:43 -0300

    Create ex20.py

---
+++ secao04/ex20.py
@@ -0,0 +1,6 @@
+#20. Leia um valor de massa em quilogramas e apresente-o convertido em libras. A fórmula
+# de conversão é: L = K/0,45 ,sendo K a massa em qulogramas e L a massa em livras.
+
+k = float(input("Digite o valor de quilogramas: "))
+l = k/0.45
+print(l)
