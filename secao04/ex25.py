4ff69f0483d5821a1ce96eb41aa7670ed12875bb
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 13:36:44 -0300

    Create ex25.py

---
+++ secao04/ex25.py
@@ -0,0 +1,6 @@
+#25.  Leia um valor de área em acres e apresente-o convertido em metros quadrados m². A
+# fórmula de conversão é: M = A * 4048,58, sendo M a área em metros quadrados e A a 
+# área em acres
+ a = float(input('Digite em acres para converter em metros quadrados: '))
+ m = a * 4048.58
+ print('{0} jardas = {1} metros quadrados'.format(a,m))
