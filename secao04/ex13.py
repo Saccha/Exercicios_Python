b658b058e8c1c2de6da3092da9db0cbca6b5767a
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 01:27:05 -0300

    Create ex13.py

---
+++ secao04/ex13.py
@@ -0,0 +1,5 @@
+""" 13. Leia uma distância em quilómetros e apresente-a convertida em milhas. A fórmula de
+conversão é: M = k/1.61, sendo K a distância em quilômetros e M em milhas."""
+k = float(input("Digite o valor de K: "))
+m = k / 1.61
+print(m)
