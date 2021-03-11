70a17319206d706e05838a42250fde1a14a6818c
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 15:40:17 -0300

    Create ex34.py

---
+++ secao04/ex34.py
@@ -0,0 +1,6 @@
+#34. Leia o valor do raio de um círculo e calcule e imprima a área do circulo correspondente.
+
+
+R = int(input('Digite o valor do raio do círculo para saber sua área: '))
+A = 3.14 * R**2
+print('A área de um círculo com {0} de raio é {1} '.format(R,A))
