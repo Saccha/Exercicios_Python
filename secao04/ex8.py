ef7d4a7b63aeb46e842d0c4c831512830ed8b5a8
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-11 00:34:33 -0300

    Create ex8.py

---
+++ secao04/ex8.py
@@ -0,0 +1,5 @@
+""" 8. Leia uma temperatura em graus Kelvin e apresente-a convertida em gruas Celsius. A fórmula de conversão é:  
+C = k - 273.15, sendo C a temperatura em Celsius e K a temperatura em Kelvin"""
+k = float(input("Digite o valor: "))
+c = k - 273.15
+print(c)
