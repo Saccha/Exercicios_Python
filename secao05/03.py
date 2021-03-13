10d5006525fbcf63e7912e01869b1a418e492d83
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-13 17:55:46 -0300

    Create 03.py

---
+++ secao05/03.py
@@ -0,0 +1,13 @@
+'''
+3. Leia um número real. Se o número for positivo imprima a raiz quadrada. Do contrário,
+imprima o número ao quadrado.
+'''
+
+x = int(input("Digite o valor de x: "))
+
+if ( x > 0 ):
+  raiz = int(x ** (1/2))
+  print(f"O número {x} é positivo e sua raiz quadrada é {raiz}")
+else:
+  quad = x ** 2
+  print(f"O número {x} é negativo e este número ao quadrado é {quad}") 
