ea86ea96424d610359e030ebe5f973f9ae8ddc40
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-12 22:29:41 -0300

    Create ex45.py

---
+++ secao04/ex45.py
@@ -0,0 +1,5 @@
+#45. Faça um programa para converter uma letra maiúscula em letra minúscula. Use a tavela 
+# ASCII para resolver o problema.
+
+letra_minuscula = str(input('Digite em letras minúsculas para converte em letras maiúscula: '))
+print(letra_minuscula.swapcase())
