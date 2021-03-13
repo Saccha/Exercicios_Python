8dbbf2eb7ce44e80abb4bd2f7e6df62f5f264b48
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-13 17:53:54 -0300

    Create ex01.py

---
+++ secao05/ex01.py
@@ -0,0 +1,11 @@
+'''
+1. Faça um programa que recba dois números e mostre qual deles é o maior 
+'''
+
+x = int(input("Digite o valor de x: "))
+y = int(input("Digite o valor de y: "))
+
+if (x > y):
+  print("X é maior que y")
+else:
+  print("X é menor que Y")
