340d5c57c3bc75b50c5abf9584e4a59441636981
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-14 22:46:50 -0300

    Create ex30.py

---
+++ secao05/ex30.py
@@ -0,0 +1,12 @@
+"""
+30. Faça um programa que receba três números e mostre-os em ordem crescente.
+"""
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+numero3 = int(input("Digite o terceiro número: "))
+
+lista = [numero1, numero2, numero3]
+
+print(lista.sort())
+print(lista)
