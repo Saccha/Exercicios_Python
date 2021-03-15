15fbe3edf9e7bca08aa36b5e2e8a9ab4c2d964e5
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-15 00:40:17 -0300

    Create ex06.py

---
+++ secao06/ex06.py
@@ -0,0 +1,8 @@
+"""
+6.Faça um programa que leia 10 inteiros e imprima sua média.
+"""
+
+for n in range(1,11):
+  num = int(input("Digite um número: "))
+  soma += num
+print(f'A soma total dos números digitados é {soma}. A média do total dos números digitados é {soma/5}')
