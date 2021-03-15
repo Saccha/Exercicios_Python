337d2bbc4c8a03eee835b2aea13fcf9674fa2830
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-15 00:39:12 -0300

    Create ex02.py

---
+++ secao06/ex02.py
@@ -0,0 +1,10 @@
+"""
+2. Escreva um programa que escreva na tela de 1 até 100, de 1 em 1, 2 vezes. A primeira
+vez deve usar a estrutura de repetição for, a segunda while.
+"""
+for n in range(1,101):
+  print(n)
+num = 1  
+while num <= 100:
+  print(num)
+  num += 1
