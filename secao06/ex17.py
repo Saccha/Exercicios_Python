fc8b4ec5e279935ab2e490590b07f90efa5df290
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-15 23:28:33 -0300

    Create ex17.py

---
+++ secao06/ex17.py
@@ -0,0 +1,18 @@
+"""
+17. Faça um programa que leia um número inteiro positivo
+n e calcule a soma dos n primeiros números naturais.
+"""
+
+num = int(input("Digite um número inteiro e positivo: "))
+soma = 0
+print()
+if num > 0:
+    print(f"A soma dos {num} primeiros números naturais:")
+    for i in range(1, num*num):
+        if i <= num:
+            soma += i
+        else:
+            break
+    print(soma)
+else:
+    print("O número não pode ser zero ou negativo!")
