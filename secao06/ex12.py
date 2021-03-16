01dfba2289604e957b67c32ca284b5c7601a4531
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-15 23:11:53 -0300

    Create ex12.py

---
+++ secao06/ex12.py
@@ -0,0 +1,9 @@
+'''
+12. Faça um programa que leia um número inteiro positivo N e imprima todos os números naturais de 0 até N
+em ordem decrescente
+'''
+
+x = int(input("Digite um número inteiro positivo: "))
+while x >= 0:
+  print(x)
+  x -= 1
