10f66feac9d096ece80b2e8fa6ceaf2f8eef9330
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-15 23:28:52 -0300

    Create ex18.py

---
+++ secao06/ex18.py
@@ -0,0 +1,15 @@
+"""
+18. Escreva um algoritmo que leia certa quantidade de números
+e imprima o maior deles e quantas vezes o maior número foi lido.
+A quantidade de números a serem lidos deve ser fornecida pelo usuário.
+"""
+
+n = int(input("Digite a quantidade de números a serem lidos: "))
+maior = 0
+for i in range(1, n+1):
+    num = int(input(f"Digite o {i}° número: "))
+    if i == 1:
+        maior = num
+    elif num > maior:
+        maior = num
+print(f"O maior número: {maior}")
