837380a1d773451cbc180bfd89c1013064377045
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-30 23:45:12 -0300

    Create ex41.py

---
+++ secao08/ex41.py
@@ -0,0 +1,6 @@
+'''
+41.Faça uma função que receba um vetor de inteiros e retorne o maior valor.
+'''
+def maior(*args):
+  return max(args)
+print(maior(10,20,23,100,120,130,-1,4,98,213,678,34,21))
